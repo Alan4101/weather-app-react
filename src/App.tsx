ffed44ba3253698338e899386d1805bb3dcf423f
Andrew Tolochkevych <andriy410tolochkevitch@gmail.com>
2022-09-21 23:31:39 +0300

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import { MainWeather, WeatherList } from './components';
 export const App:FC = () => {
   return (
     <div className="App">
+      {/* <video className='video-bg' autoPlay muted>
+          <source type='video/mp4' src='/cloudy.mp4'/>
+        </video> */}
       <div className='mainWrapper'>
+        
         <div className='weather__container'>
           <MainWeather/>
         </div>
